b6ce69a266ca6f01ad78a8f2396b505b2c300d81
Balnur6 <talgat.balnur@jihc.kz>
2024-05-08 23:15:25 +0500

    food delivery app

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:full_food/auth/login_or_register.dart';
+import 'package:full_food/models/restaurant.dart';
+import 'package:full_food/themes/theme_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(
+    MultiProvider(
+      providers: [
+        //theme provider
+        ChangeNotifierProvider(create: (context) => ThemeProvider()),
+
+        //restaurant provider
+        ChangeNotifierProvider(create: (context) => Restaurant()),
+      ],
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const LoginOrRegister(),
+      theme: Provider.of<ThemeProvider>(context).themeData,
+    );
+  }
+}
